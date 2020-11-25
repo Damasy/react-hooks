867c3baeff96ba8343d6d360261d0a9701b99ef2
903601 <super.nova.egy@gmail.com>
2020-11-25 23:11:36 +0200

    useCallback hook

--- src/App.js
+++ src/App.js
@@ -1,51 +1,60 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef, useCallback } from "react";
+import Button from "./Button";
 
 export default function App() {
   const [counter, setCounter] = useState(0);
-  const [title, setTitle] = useState('');
+  const [title, setTitle] = useState("");
   const amountRef = useRef();
-  const reachMaxRef = useRef(false);
+  // const reachMaxRef = useRef(false);
+
+  const listofCounts = [1, 10, 20, 40, 50];
 
   // method - must bind this context inside constructor to solve this context issue
-  function countUp() {
-    if(!reachMaxRef.current) {
-      if(counter >= 10) {
-        reachMaxRef.current = true;
-      } else {
-        setCounter(c => c + (+amountRef.current.value || 1));
-      }
-    } else console.log('You have reached max clicks')
-  }
+  // function countUp() {
+  //   if (!reachMaxRef.current) {
+  //     if (counter >= 10) {
+  //       reachMaxRef.current = true;
+  //     } else {
+  //       setCounter((c) => c + (+amountRef.current.value || 1));
+  //     }
+  //   } else console.log("You have reached max clicks");
+  // }
 
   function changeTitle() {
-    setTitle('Welcome to React Hooks')
+    setTitle("Welcome to React Hooks");
   }
 
   useEffect(() => {
-    console.log('inside useEffect 1')
+    // console.log('inside useEffect 1')
     document.title = title;
 
     // clear function
     return () => {
-      console.log('clear')
-    }
-  }, [title])
+      console.log("clear");
+    };
+  }, [title]);
 
   useEffect(() => {
-    console.log('inside useEffect 2')
+    // console.log('inside useEffect 2')
     document.title = `You clicked ${counter} times`;
-  }, [counter])
+  }, [counter]);
 
   useEffect(() => {
     amountRef.current.focus();
-  })
-  
+  });
+
+  const onClick = useCallback((n) => setCounter((c) => c + n), [setCounter]);
+
   return (
     <div className="container text-center mt-5">
       <input ref={amountRef} />
       <div className="mt-3">{counter}</div>
-      <button className="btn btn-primary mx-2 mt-3" onClick={countUp}>Count Up</button>
-      <button className="btn btn-success mx-2 mt-3" onClick={changeTitle}>Change Title</button>
+      {listofCounts.map((count) => (
+        <Button countUp={onClick} label={count} key={count} n={count} />
+      ))}
+      <button className="btn btn-success mx-2 mt-3" onClick={changeTitle}>
+        Change Title
+      </button>
     </div>
-  )
+  );
 }
---
+++ src/Button.js
@@ -0,0 +1,11 @@
+import React, { useRef, memo } from 'react'
+
+const Button = (props) => {
+  const renderRef = useRef(0);
+  console.log(renderRef.current++)
+  return (
+  <button className="btn btn-primary mx-2 mt-3" onClick={() => props.countUp(props.n)}>{props.label}</button>
+  )
+}
+
+export default memo(Button)
